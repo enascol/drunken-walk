3d7f14155ef9ba52514c24c9d02fb19e3384bd27
Erick Patrick <erick.nascol@gmail.com>
2022-07-13 22:51:03 -0300

    remove unused function, rename class name and fix padding

--- procgen.py
+++ procgen.py
@@ -3,7 +3,7 @@ import sys
 from PIL import Image
 import numpy as np
 
-class Cave:
+class Matrix:
 	FILLED_POINT_TILE = "#"
 	EMPTY_POINT_TILE = " "
 	PADDING = 2
@@ -16,7 +16,7 @@ class Cave:
 		self.matrix = self.generate_matrix()
 
 	def generate_matrix(self):
-		return [[Cave.FILLED_POINT_TILE] * self.columns for _ in range(self.rows)]
+		return [[Matrix.FILLED_POINT_TILE] * self.columns for _ in range(self.rows)]
 	
 	def get_midway_position(self):
 		return int(self.rows / 2), int(self.columns / 2)
@@ -26,13 +26,13 @@ class Cave:
 
 		roll = random.randint(1, 4)
 
-		if roll == 1 and x > Cave.PADDING:
+		if roll == 1 and x > Matrix.PADDING:
 			x -= 1
-		if roll == 2 and x < self.rows - Cave.PADDING:
+		if roll == 2 and x < self.rows - Matrix.PADDING:
 			x += 1
-		if roll == 3 and y > Cave.PADDING:
+		if roll == 3 and y > Matrix.PADDING:
 			y -= 1
-		if roll == 4 and y < self.columns - Cave.PADDING:
+		if roll == 4 and y < self.columns - Matrix.PADDING:
 			y += 1
 
 		return x, y
@@ -41,7 +41,12 @@ class Cave:
 		if tile_type:
 			while True:
 				x, y = self.get_random_position()
-				if self.matrix[x][y] == tile_type:
+				
+				x_not_on_padding = x > Matrix.PADDING and x < self.rows - Matrix.PADDING
+				y_not_on_padding = y > Matrix.PADDING and y < self.columns - Matrix.PADDING
+				match_correct_tile_type = self.matrix[x][y] == tile_type
+				
+				if x_not_on_padding and y_not_on_padding and match_correct_tile_type:
 					return x, y
 		else:
 			return random.randint(0, self.rows - 1), random.randint(0, self.columns -1)
@@ -51,21 +56,21 @@ class Cave:
 		print(amount)
 		x, y = self.get_midway_position()
 
-		if Cave.MAX_WALK_BEFORE_JUMP == 0:
+		if Matrix.MAX_WALK_BEFORE_JUMP == 0:
 			max_walk = max_empty_tiles
 		else:
-			max_walk = Cave.MAX_WALK_BEFORE_JUMP
+			max_walk = Matrix.MAX_WALK_BEFORE_JUMP
 		
 		count = 0
 
 		while amount:
-			if self.matrix[x][y] == Cave.FILLED_POINT_TILE:
-				self.matrix[x][y] = Cave.EMPTY_POINT_TILE
+			if self.matrix[x][y] == Matrix.FILLED_POINT_TILE:
+				self.matrix[x][y] = Matrix.EMPTY_POINT_TILE
 				amount -= 1
 				count += 1
 
 			if count == max_walk:
-				x, y = self.get_random_position(tile_type=Cave.FILLED_POINT_TILE)
+				x, y = self.get_random_position(tile_type=Matrix.FILLED_POINT_TILE)
 				count = 0
 			else:
 				x, y = self.get_new_position(x, y)
@@ -82,40 +87,13 @@ class Cave:
 
 		for x in range(self.rows):
 			for y in range(self.columns):
-				if self.matrix[x][y] == Cave.EMPTY_POINT_TILE:
+				if self.matrix[x][y] == Matrix.EMPTY_POINT_TILE:
 					matrix[x][y] = (0, 0, 0)
 		
 		matrix = np.asarray(matrix, dtype=np.uint8)
 		img = Image.fromarray(matrix)	
 
 		img.save(path)
-	
-	def configure(self, settings):
-		settings = convert_string_to_settings(",".join(settings))
-		options = [
-			("empty_tile", " "), 
-			("filled_tile", "#"), 
-			("walk_before_jump", 0),
-			("padding", 2),
-			("max_empty", 1),
-			("rows", 10),
-			("columns", 10) 
-		]
-
-		for option in options:
-			o, v  = option
-			if o not in settings:
-				settings[o] = v
-		
-		Cave.EMPTY_POINT_TILE = settings["empty_tile"]
-		Cave.FILLED_POINT_TILE = settings["filled_tile"]
-		Cave.MAX_WALK_BEFORE_JUMP = int(settings["walk_before_jump"])
-		Cave.PADDING = int(settings["padding"])
-		self.rows = int(settings["rows"])
-		self.columns = int(settings["columns"])
-		settings["max_empty"] = int(settings["max_empty"])
-		
-		return settings
 
 def convert_string_to_settings(settings):
 	parsed_1 = [option.strip() for option in settings.split(",")]
@@ -126,8 +104,8 @@ ROWS, COLUMNS, MAX, JUMP_BEFORE_WALK = range(4)
 
 settings = [int(x) for x in sys.argv[1:]]
 print(settings)
-cave = Cave(settings[ROWS], settings[COLUMNS])
-Cave.MAX_WALK_BEFORE_JUMP = settings[JUMP_BEFORE_WALK]
+cave = Matrix(settings[ROWS], settings[COLUMNS])
+Matrix.MAX_WALK_BEFORE_JUMP = settings[JUMP_BEFORE_WALK]
 cave.generate(settings[MAX])
 cave.show()
 cave.convert_to_img("oii.png")
